e28ffc4368669486c3165273d96ba731c7ba6e05
farookfazni <faznifarook@gmail.com>
2023-06-08 13:39:31 +0530

    added the model

--- FindKeyword.py
+++ FindKeyword.py
@@ -1,8 +1,11 @@
 import re
+def FindKeyWords(keywords, text):
+    highlighted_text = text
 
-def FindKeyWord(keyword, text):
-    if re.search(r'\b({0})\b'.format(re.escape(keyword)), text, flags=re.IGNORECASE):
-        highlighted_text = re.sub(r'\b({0})\b'.format(re.escape(keyword)), r'<mark style="background-color: yellow;">\1</mark>', text, flags=re.IGNORECASE)
-        return highlighted_text
-    else:
-         return "Keyword not found in the Resume."
+    for keyword in keywords:
+        if re.search(r'\b({0})\b'.format(re.escape(keyword)), highlighted_text, flags=re.IGNORECASE):
+            highlighted_text = re.sub(r'\b({0})\b'.format(re.escape(keyword)), r'<mark style="background-color: yellow;">\1</mark>', highlighted_text, flags=re.IGNORECASE)
+        else:
+            return "Keyword not found in the Resume."
+
+    return highlighted_text
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from PyPDF2 import PdfReader
+from FindKeyword import FindKeyWords
 from PreprocessText import preprocess_text
-from FindKeyword import FindKeyWord
+from model_Responce import model_prediction
 from streamlit_extras.add_vertical_space import add_vertical_space
 
 # Sidebar contents
@@ -12,19 +13,49 @@ with st.sidebar:
     This app is an LLM-powered chatbot built using:
     - [Streamlit](https://streamlit.io/)
     - [LangChain](https://python.langchain.com/)
-    - [OpenAI](https://platform.openai.com/docs/models) LLM model 
+    - Custom Trained AI model 
  
     ''')
     add_vertical_space(5)
     st.write('Made with ❤️ by Fazni Farook')
 
+def button_function(all_text):
+    # Add your desired functionality here
+    # predictions = []
+    for item in all_text:
+        text = item['text']
+        # filename = item['filename']
+        pred = model_prediction(text)
+        # predictions.append({"filename": filename, "prediction": pred})
+        item['prediction'] = pred
+    # st.write(predictions)
+    return all_text
+
+def filter_keywords(all_text, keywords):
+    filtered_text = []
+    for item in all_text:
+        filename = item['filename']
+        text = item['text']
+        filtered_text_with_keywords = FindKeyWords(keywords, text)
+        filtered_text.append({"filename": filename, "text": filtered_text_with_keywords})
+    return filtered_text
+
+            
 # Main body
 def main():
     st.header("Resume Filter using Keywords 💬")
 
     # upload a PDF file
     pdfs = st.file_uploader("Upload your Resumes", type='pdf',accept_multiple_files=True)
-    
+
+
+
+    # Get user preference for matching keywords
+    # match_all_keywords = st.checkbox("Match All Keywords")
+
+     # Choose functionality: Prediction or Filtering
+    functionality = st.radio("Choose functionality:", ("Make Predictions", "Filter Keywords"))
+
     if pdfs is not None:
         all_text = []
         for pdf in pdfs:
@@ -42,16 +73,35 @@ def main():
                 text += page.extract_text()
             # Preprocess the text
             text = preprocess_text(text)
-            keyword = "Computer Science"
-            Text_with_Keyword = FindKeyWord(keyword, text)
             # Appending to array
-            all_text.append({"filename": filename, "text": Text_with_Keyword})
-        # st.write(all_text, unsafe_allow_html=True)
-        # st.markdown(all_text, unsafe_allow_html=True)
-        for item in all_text:
-            st.markdown(f"**Filename: {item['filename']}**")
-            st.markdown(item['text'], unsafe_allow_html=True)
-            st.markdown("---")
+            all_text.append({"filename": filename, "text": text})
+
+        if functionality == "Make Predictions":
+            if st.button('Make Prediction'):
+                all_text = button_function(all_text)
+
+                for item in all_text:
+                    filename = item["filename"]
+                    text = item["text"]
+                    pred = item["prediction"]
+                    st.markdown(f"**Filename: {filename}**")
+                    st.markdown(text, unsafe_allow_html=True)
+                    st.markdown(f"**Prediction: {pred}**")
+                    st.markdown("---")
+        elif functionality == "Filter Keywords":
+            # getting the keywords
+            keyword_input  = st.text_input("Keyword")
+            keywords = [keyword.strip() for keyword in keyword_input.split(",")]
+
+            if st.button('Filter Keywords'):
+                filtered_text = filter_keywords(all_text, keywords)
+
+                for item in filtered_text:
+                    filename = item["filename"]
+                    text = item["text"]
+                    st.markdown(f"**Filename: {filename}**")
+                    st.markdown(text, unsafe_allow_html=True)
+                    st.markdown("---")
 
 if __name__=='__main__':
     main()
