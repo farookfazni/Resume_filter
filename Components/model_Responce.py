8f6c72f054e883bc038a76c0f1f1f889df2b82e9
farookfazni <faznifarook@gmail.com>
2023-12-30 13:47:53 +0530

    made some important changes and fixed some bugs

--- Components/model_Responce.py
+++ Components/model_Responce.py
@@ -7,55 +7,59 @@ from keras.utils import pad_sequences
 from keras.preprocessing.text import Tokenizer
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-# Load the model from the pickle file
-# filename = 'F:/CVFilter/models/model_pk.pkl'
-# with open(filename, 'rb') as file:
-#     model = pickle.load(file)
-
-# Load the saved model
-# model = joblib.load('F:\CVFilter\models\model.joblib')
-
+# Transformer Model
 # Define the model name
 model_name = "fazni/distilbert-base-uncased-career-path-prediction"
-
 
 # Load the model
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
-# Local model
-# model = tf.keras.models.load_model('models\model.h5')
-
 # Load the tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-# Local Tokenizer
-# tokenfile = 'tokenized_words/tokenized_words.pkl'
-# # Load the tokenized words from the pickle file
-# with open(tokenfile, 'rb') as file:
-#     loaded_tokenized_words = pickle.load(file)
-
-# max_review_length = 200
-# tokenizer = Tokenizer(num_words=10000,  #max no. of unique words to keep
-#                       filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', 
-#                       lower=True #convert to lower case
-#                      )
-# tokenizer.fit_on_texts(loaded_tokenized_words)
 
 outcome_labels = ['Business Analyst', 'Cyber Security','Data Engineer','Data Science','DevOps','Machine Learning Engineer','Mobile App Developer','Network Engineer','Quality Assurance','Software Engineer']
 
 def model_prediction(text, model=model, tokenizer=tokenizer, labels=outcome_labels):
-    # for local model
-    # seq = tokenizer.texts_to_sequences([text])
-    # padded = pad_sequences(seq, maxlen=max_review_length)
-    # pred = model.predict(padded)
 
+    # Hugging face model
     # Tokenize the text
-    inputs = tokenizer(text, return_tensors="pt")
+    inputs = tokenizer(text, return_tensors="pt",truncation=True, max_length=512)
     outputs = model(**inputs)
     
     # Get the predicted class probabilities
     probs = outputs.logits.softmax(dim=-1)
 
-    # print("Probability distribution: ", pred)
-    # print("Field ")
     return labels[torch.argmax(probs)]
+
+
+def model_prediction_local_model(text,labels=outcome_labels):
+    # Load the model from the pickle file
+    # filename = 'F:/CVFilter/models/model_pk.pkl'
+    # with open(filename, 'rb') as file:
+    #     model = pickle.load(file)
+
+    # Load the saved model
+    # model = joblib.load('F:\CVFilter\models\model.joblib')
+    
+    # Local model
+    model = tf.keras.models.load_model('models\model.h5')
+
+    # Local Tokenizer
+    tokenfile = 'tokenized_words/tokenized_words.pkl'
+    # Load the tokenized words from the pickle file
+    with open(tokenfile, 'rb') as file:
+        loaded_tokenized_words = pickle.load(file)
+
+    max_review_length = 200
+    tokenizer = Tokenizer(num_words=10000,  #max no. of unique words to keep
+                          filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', 
+                          lower=True #convert to lower case
+                         )
+    tokenizer.fit_on_texts(loaded_tokenized_words)
+
+    # for local model
+    seq = tokenizer.texts_to_sequences([text])
+    padded = pad_sequences(seq, maxlen=max_review_length)
+    pred = model.predict(padded)
+
+    return labels[np.argmax(pred)]
